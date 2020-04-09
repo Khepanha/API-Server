a0da77adfb2d39a1e4e7a842229f79cd558a6ce7
panha <khepanha18@kit.edu.kh>
2020-04-09 14:09:10 +0700

    New Commit

--- API Project/Express MyApp/app.js
+++ API Project/Express MyApp/app.js
@@ -1,12 +1,8 @@
-// import http from 'http';
-// import express from 'express';
-// import logger from 'morgan';
-// import bodyParser from 'body-parser';
-
 var http = require('http');
 var express = require('express');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var routes = require('./server/routes');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -18,6 +14,8 @@ app.use(logger('dev')); // log requests to the console
 // Parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+routes(app);
 
 app.get('*', (req, res) => res.status(200).send({
     message: 'Welcome to the default API route',
---
+++ API Project/Express MyApp/server/routes/index.js
@@ -0,0 +1,13 @@
+// import Users from '../controllers/user';
+// import Books from '../controllers/book';
+var Users = require('../controllers/user');
+var Books = require('../controllers/book');
+
+export default (app) => {
+    app.get('/api', (req, res) => res.status(200).send({
+    message: 'Welcome to the BookStore API!',
+    }));
+
+    app.post('/api/users', Users.signUp); // API route for user to signup
+    app.post('/api/users/:userId/books', Books.create); // API route for user to create a book
+};
